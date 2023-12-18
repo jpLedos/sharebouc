6049e37a58b1ef41cd5289bb20567badc6d80ea8
Jean-Pierre Ledos <jpledos@gmail.com>
2023-12-18 18:54:32 +0100

    CLEAN :Css-fixer

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace App\DataFixtures;
 
 use App\Entity\Author;
@@ -16,7 +25,7 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $faker = Faker::create ('fr_FR');
+        $faker = Faker::create('fr_FR');
 
         // $userArray = ["admin@test.fr", "admin", "admin"];
         // $admin = new User;
@@ -26,73 +35,73 @@ class AppFixtures extends Fixture
         // $manager->persist($admin);
 
         // creation des status
-        $statusArray = ["A lire", "En cours", "Lu", "Abandonné"];
-        $allStatus=[];
-        for ($i = 0; $i < count($statusArray) ; $i++) {
-            $status =new Status;
+        $statusArray = ['A lire', 'En cours', 'Lu', 'Abandonné'];
+        $allStatus = [];
+        for ($i = 0; $i < \count($statusArray); ++$i) {
+            $status = new Status();
             $status->setName($statusArray[$i]);
             $manager->persist($status);
             $allStatus[] = $status;
         }
 
         // creation de genres litteraires
-        $kinds = ["Roman", "Policier", "Bande dessinée", "Science-fiction","Compte"];
-        $allKinds=[];
-        for ($i = 0; $i < count($kinds) ; $i++) {
-            $kind =new Kind;
+        $kinds = ['Roman', 'Policier', 'Bande dessinée', 'Science-fiction', 'Compte'];
+        $allKinds = [];
+        for ($i = 0; $i < \count($kinds); ++$i) {
+            $kind = new Kind();
             $kind->setName($kinds[$i]);
             $kind->setColor($faker->hexColor());
             $kind->setBgColor($faker->hexColor());
             $manager->persist($kind);
-            $allKinds[]=$kind;
+            $allKinds[] = $kind;
         }
 
         // creation des auteurs
         $authors = [];
-        for ($i = 0; $i < 10 ; $i++) {
-            $author =new Author;
+        for ($i = 0; $i < 10; ++$i) {
+            $author = new Author();
             $author->setName($faker->name);
             $manager->persist($author);
-            $authors[]=$author;
+            $authors[] = $author;
         }
 
-        //creation des utilisateurs
+        // creation des utilisateurs
         $users = [];
-        for ($i = 0; $i < 10 ; $i++) {
-            $user =new User;
+        for ($i = 0; $i < 10; ++$i) {
+            $user = new User();
             $user->setEmail($faker->email);
             $user->setPseudo($faker->username);
             $user->setPassword($faker->password);
             $manager->persist($user);
-            $users[]=$user;
+            $users[] = $user;
         }
 
         // creation des livres
-        $books=[];
-        for ($i = 0; $i < 50 ; $i++) {
-            $book =new Book;
+        $books = [];
+        for ($i = 0; $i < 50; ++$i) {
+            $book = new Book();
             $book
                 ->setgoogleId($faker->uuid())
                 ->setTitle($faker->sentence)
                 ->setDescription($faker->text)
-                ->setPageCount($faker->numberBetween(200,800))
-                ->setThumbnail($faker->imageUrl(200,300))
+                ->setPageCount($faker->numberBetween(200, 800))
+                ->setThumbnail($faker->imageUrl(200, 300))
                 ->setAddedAt(new \DateTimeImmutable())
                 ->setAddedBy($faker->randomElement($users))
                 ->addKind($faker->randomElement($allKinds))
                 ->addAuthor($faker->randomElement($authors));
             $manager->persist($book);
-            $books[]=$book;
+            $books[] = $book;
         }
 
-        //creation des liste de lectures
-        foreach($users as $user) {
-            for ($i=0; $i < 10; $i++ ) {
-                $userBook = new UserBook;
+        // creation des liste de lectures
+        foreach ($users as $user) {
+            for ($i = 0; $i < 10; ++$i) {
+                $userBook = new UserBook();
                 $userBook
                     ->setUser($user)
                     ->setStatus($faker->randomElement($allStatus))
-                    ->setNote($faker->numberBetween(1,5))
+                    ->setNote($faker->numberBetween(1, 5))
                     ->setCommentTitle($faker->sentence(3))
                     ->setComment($faker->text)
                     ->setCreateAt(new \DateTimeImmutable())
